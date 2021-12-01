77f57bce1ce25640cb0000d35a4d22595bd95520
Jose Servin <Servinjose96@gmail.com>
2021-12-01 10:55:10 -0600

    Added option to make a new graph based off user input

--- Random_Walk_Visual.py
+++ Random_Walk_Visual.py
@@ -1,10 +1,15 @@
 import matplotlib.pyplot as plt
 from Random_Walk import RandomWalk
 
+while True:
 # Make a random walk and plot the points
-rw = RandomWalk() # creating a Class instance 
-rw.fill_walk() # using the fill_walk() method
+    rw = RandomWalk() # creating a Class instance 
+    rw.fill_walk() # using the fill_walk() method
+    plt.scatter(rw.x_values, rw.y_values, s=5)
+    plt.show()
 
-plt.scatter(rw.x_values, rw.y_values, s=5)
-plt.show()
+    keep_running = input("Do you want to generate a new graph? y/n ")
+    if keep_running == 'n':
+        break
+    
 
