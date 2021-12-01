43e5b7cf2f5f12798b6ee171c27e8da08523ca80
Jose Servin <Servinjose96@gmail.com>
2021-12-01 16:58:19 -0600

    Modified point count to 50000 and added a colormap to randomly generated route taken. Also made start and stop point stand out

--- Random_Walk_Visual.py
+++ Random_Walk_Visual.py
@@ -3,9 +3,19 @@ from Random_Walk import RandomWalk
 
 while True:
 # Make a random walk and plot the points
-    rw = RandomWalk() # creating a Class instance 
+    rw = RandomWalk(50000) # creating a Class instance 
     rw.fill_walk() # using the fill_walk() method
-    plt.scatter(rw.x_values, rw.y_values, s=5)
+
+    # Introducing a colormap 
+    point_numbers = list(range(rw.number_points))
+    plt.scatter(rw.x_values, rw.y_values, c =point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=3)
+
+    # Emphasize starting and ending point
+    plt.scatter(0,0, c='green', edgecolors='none', s=50)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s= 50)
+
+    plt.axis('off')
+
     plt.show()
 
     keep_running = input("Do you want to generate a new graph? y/n ")
