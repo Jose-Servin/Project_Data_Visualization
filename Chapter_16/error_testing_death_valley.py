12d8ee8bc115ca2b56e1d73f6b838454f0b3dad7
Jose Servin <Servinjose96@gmail.com>
2021-12-05 22:26:19 -0600

    completed chapter 16 workthrough

--- Chapter_16/error_testing_death_valley.py
+++ Chapter_16/error_testing_death_valley.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import matplotlib.pyplot as plt
 
 # Import file
-file_path = ('Chapter_16/sitka_weather_2014.csv')
+file_path = ('Chapter_16/death_valley_2014.csv')
 with open(file_path) as file:
     reader = csv.reader(file)
     headers = next(reader)
@@ -14,22 +14,23 @@ with open(file_path) as file:
     min_temps = []
     dates = []
     for row in reader:
-        temp_max = int(row[1])
-        max_temps.append(temp_max)
-
-        temp_min = int(row[3])
-        min_temps.append(temp_min)
-
-        row_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(row_date)
-
+        try:
+            date = datetime.strptime(row[0], "%Y-%m-%d")
+            temp_max = int(row[1])
+            temp_min = int(row[2])
+        except ValueError:
+            print(date, ' is missing data.')
+        else:
+            max_temps.append(temp_max)
+            min_temps.append(temp_min)
+            dates.append(date)
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dates, max_temps, c='red')
 plt.plot(dates, min_temps, c='blue')
 plt.fill_between(dates, max_temps, min_temps, facecolor = 'blue', alpha = 0.1)
-plt.title("Yearly Sitka Max and Min Temperatures for 2014")
+plt.title("Yearly Death Valley Min/Max Temperatures for 2014")
 plt.xlabel("Date")
-plt.ylabel("Max and Min Temperature F")
+plt.ylabel("Min/Max Temperature F")
 fig.autofmt_xdate()
 plt.tick_params(axis='both', which='major', labelsize=16)
 plt.show()
